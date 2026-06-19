cb72c1c34cff26f3fd1ac5191367edcad2380fdf
kantanvgat <kantan.vgat@gmail.com>
2026-06-19 22:15:49 +0900

    [update] キャラクターのanimationの処理を追加

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,9 +5,12 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] float _playerSpeed = 2f;
     private Rigidbody _rb;
     [SerializeField] public bool _isMoving = true;
+
+    Animator _animator;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -16,14 +19,26 @@ public class PlayerMove : MonoBehaviour
         {
             Move();
         }
+        Anima();
     }
-
-    void Move()
+    
+    void Move() //移動処理
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
         Vector3 move = new Vector3(x,0,z);
-        transform.Translate(move * _playerSpeed * Time.deltaTime);
+        //transform.Translate(move * _playerSpeed * Time.deltaTime);
+        _rb.MovePosition(_rb.position + move * _playerSpeed * Time.fixedDeltaTime);
+    }
+
+    void Anima() //アニメーションの処理
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        float speed = new Vector2(x, z).magnitude;
+
+        _animator.SetFloat("Speed", speed);
     }
 }
